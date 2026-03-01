f8fcfd0aaaaea82f86c46130f0dd069b5acfa09f
trumpvpn-tg <firum35@gmail.com>
2026-03-01 19:29:05 +0300

    Create  sw.js

---
+++ sw.js
@@ -0,0 +1,30 @@
+var CACHE = 'tvpn-v1';
+var PRECACHE = ['./', './manifest.json'];
+
+self.addEventListener('install', function(e) {
+  e.waitUntil(
+    caches.open(CACHE).then(function(c) { return c.addAll(PRECACHE); })
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', function(e) {
+  e.waitUntil(
+    caches.keys().then(function(keys) {
+      return Promise.all(
+        keys.filter(function(k) { return k !== CACHE; })
+            .map(function(k) { return caches.delete(k); })
+      );
+    })
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', function(e) {
+  if (e.request.url.includes('/api/')) return;
+  e.respondWith(
+    fetch(e.request).catch(function() {
+      return caches.match(e.request);
+    })
+  );
+});
